8cef9e20b513dc11e4d0d45aff456ec69807090c
xwp <xwp_fullstack@163.com>
2018-07-20 20:39:31 +0800

    7.20

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,79 +1,100 @@
-from flask import render_template, current_app, session, request, jsonify
-from info import redis_store, constants
-from info.models import User, News
+from flask import render_template, current_app, request, jsonify, g
+from info import constants
+from info.models import News, Category
+from info.utils.common import user_login_data
 from info.utils.response_code import RET
 from . import index_blu
 
 
-@index_blu.route("/news_list", methods=["GET"])
+@index_blu.route("/news_list")
 def news_list():
     """显示首页滚动新闻和分类新闻展示"""
+    # 1. 获取参数
+    # 新闻的分类id
     cid = request.args.get("cid", "1")
     page = request.args.get("page", "1")
     per_page = request.args.get("per_page", "10")
+
+    # 2. 校验参数
     try:
         cid = int(cid)
         page = int(page)
         per_page = int(per_page)
-    except Exception as error:
-        current_app.logger.error(error)
+    except Exception as e:
+        current_app.logger.error(e)
         return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
-    filters = []
-    if cid != 1:
+
+    filters = [News.status == 0]
+    if cid != 1:  # 查询的不是最新的数据
+        # 需要添加条件
         filters.append(News.category_id == cid)
+
+    # 3. 查询数据
     try:
         paginate = News.query.filter(*filters).order_by(News.create_time.desc()).paginate(page, per_page, False)
-    except Exception as error:
-        current_app.logger.error(error)
-        return jsonify(errno=RET.DBERR, errmsg="查询失败")
-    news_models_list = paginate.items
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据查询错误")
+
+    # 取到当前页的数据
+    news_model_list = paginate.items  # 模型对象列表
     total_page = paginate.pages
     current_page = paginate.page
+
+    # 将模型对象列表转成字典列表
     news_dict_li = []
-    for news in news_models_list:
+    for news in news_model_list:
         news_dict_li.append(news.to_basic_dict())
+
     data = {
         "total_page": total_page,
         "current_page": current_page,
         "news_dict_li": news_dict_li
     }
+
     return jsonify(errno=RET.OK, errmsg="OK", data=data)
 
 
 @index_blu.route("/")
+@user_login_data
 def index():
     """显示首页"""
     # 1.如果用户已经登录,将当前登录用户的数据传到模板中,供模板显示
-    user_id = session.get("user_id", None)
-    user = None
-    if user_id:
-        try:
-            user = User.query.get(user_id)
-        except Exception as error:
-            current_app.logger.error(error)
+    user = g.user
 
     # 右侧的新闻排行的逻辑
     news_list = []
     try:
         # news_list是查询语句
         news_list = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
-    except Exception as error:
-        current_app.logger.error(error)
+    except Exception as e:
+        current_app.logger.error(e)
+
+    # 定义一个空的字典列表，里面装的就是字典
     news_dict_li = []
+    # 遍历对象列表，将对象的字典添加到字典列表中
     for news in news_list:
         # news是每条新闻对应的模型对象; to_basic_dict()取类对象的属性转换成字典
         news_dict_li.append(news.to_basic_dict())
+
+    # 查询分类数据，通过模板的形式渲染出来
+    categories = Category.query.all()
+    category_li = []
+    for category in categories:
+        category_li.append(category.to_dict())
+
     data = {
-        # 三元表达式
-        "user_info": user.to_dict() if user else None,
-        "news_dict_li": news_dict_li
+        "user": user.to_dict() if user else None,  # 三元表达式
+        "news_dict_li": news_dict_li,
+        "category_li": category_li
     }
 
     # 2.把查询到的用户信息传递给浏览器渲染显示
     return render_template("news/index.html", data=data)
 
 
-# send_static_file是flask去查找指定静态文件所调用的方法
-@index_blu.route("/favicon.ico")
+# 在打开网页的时候，浏览器会默认去请求根路径+favicon.ico作网站标签的小图标
+# send_static_file 是 flask 去查找指定的静态文件所调用的方法
+@index_blu.route('/favicon.ico')
 def favicon():
     return current_app.send_static_file("news/favicon.ico")
--- info/static/news/js/index.js
+++ info/static/news/js/index.js
@@ -1,50 +1,99 @@
-var currentCid = 0; // 当前分类 id
-var cur_page = 1; // 当前页
-var total_page = 1;  // 总页数
-var data_querying = true;   // 是否正在向后台获取数据
-
+let currentCid = 1; // 当前分类 id
+let cur_page = 1; // 当前页
+let total_page = 1;  // 总页数
+let data_querying = true;   // 是否正在向后台获取数据
 
 $(function () {
+    // 界面加载完成之后去加载新闻数据
+    updateNewsData();
+
     // 首页分类切换
     $('.menu li').click(function () {
-        var clickCid = $(this).attr('data-cid')
+        // 取到指定分类的cid
+        let clickCid = $(this).attr('data-cid');
+        // 遍历所有的 li 移除身上的选中效果
         $('.menu li').each(function () {
             $(this).removeClass('active')
-        })
-        $(this).addClass('active')
-
+        });
+        // 给当前分类添加选中的状态
+        $(this).addClass('active');
+        // 如果点击的分类与当前分类不一致
         if (clickCid != currentCid) {
             // 记录当前分类id
-            currentCid = clickCid
+            currentCid = clickCid;
 
             // 重置分页参数
-            cur_page = 1
-            total_page = 1
+            cur_page = 1;
+            total_page = 1;
             updateNewsData()
         }
-    })
+    });
 
     //页面滚动加载相关
     $(window).scroll(function () {
 
         // 浏览器窗口高度
-        var showHeight = $(window).height();
+        let showHeight = $(window).height();
 
         // 整个网页的高度
-        var pageHeight = $(document).height();
+        let pageHeight = $(document).height();
 
         // 页面可以滚动的距离
-        var canScrollHeight = pageHeight - showHeight;
+        let canScrollHeight = pageHeight - showHeight;
 
         // 页面滚动了多少,这个是随着页面滚动实时变化的
-        var nowScroll = $(document).scrollTop();
+        let nowScroll = $(document).scrollTop();
 
         if ((canScrollHeight - nowScroll) < 100) {
-            // TODO 判断页数，去更新新闻数据
+            // 判断页数，去更新新闻数据；data_querying 控制数据查询的状态，每次往页底拖动只加载下一页，防止一次加载多页数据但是不会全部显示出来，浪费有问题
+            if (!data_querying) {
+                data_querying = true;
+                // 当前页小于总页数
+                if (cur_page < total_page) {
+                    cur_page += 1;
+                    updateNewsData();
+                }
+            }
         }
-    })
-})
+    });
+});
 
 function updateNewsData() {
-    // TODO 更新新闻数据
+    // 更新新闻数据
+    let params = {
+        "cid": currentCid,
+        "page": cur_page
+    };
+    $.get("/news_list", params, function (resp) {
+        // 数据加载完毕，设置【正在加载数据】的变量为 false 代表当前没有在加载数据
+        data_querying = false;
+        if (resp.errno == "0") {
+            // 给总页数据赋值
+            total_page = resp.data.total_page;
+            // 代表请求成功,清除已有数据
+            // 进入网站默认显示第一页，不管是查看 最新 分类或者其它分类时，都需要清空原有内置的html模板数据
+            if (cur_page == 1) {
+                $(".list_con").html("");
+            }
+            // 添加请求成功之后返回的数据
+            // 显示数据
+            for (let i = 0; i < resp.data.news_dict_li.length; i++) {
+                let news = resp.data.news_dict_li[i];
+                let content = '<li>';
+                content += '<a href="/news/' + news.id + '" class="news_pic fl"><img src="' + news.index_image_url + '?imageView2/1/w/170/h/170"></a>';
+                content += '<a href="/news/' + news.id + '" class="news_title fl">' + news.title + '</a>';
+                content += '<a href="/news/' + news.id + '" class="news_detail fl">' + news.digest + '</a>';
+                content += '<div class="author_info fl">';
+                content += '<div class="source fl">来源：' + news.source + '</div>';
+                content += '<div class="time fl">' + news.create_time + '</div>';
+                content += '</div>';
+                content += '</li>';
+                $(".list_con").append(content)
+            }
+        }
+        else {
+            // 请求失败
+            alert(resp.errmsg);
+        }
+    })
 }
